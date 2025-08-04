a58d836b6b55d4e2815efc8dab32fcf8f753b59d
Kalli-Turban <Kh.turban@turbix.de>
2025-08-05 00:12:10 +0200

    Modulstruktur aufgeräumt: Kartenrenderer ins frontend verschoben

---
+++ markdown_karten_renderer.py
@@ -0,0 +1,9 @@
+Formatiert einen Eintrag im Kartenstil für Markdown-Anzeige
+
+Wandelt eine Liste von Einträgen in Markdown-Kartenansicht um
+
+def format_card_entry(entry):
+    Formatiert einen Eintrag im Kartenstil für Markdown-Anzeige
+
+def render_markdown_kartenansicht(eintraege):
+    Wandelt eine Liste von Einträgen in Markdown-Kartenansicht um
